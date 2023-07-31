d25b9525884516b487483bed4e84607afaa1e4a1
loic564492 <loic.legarrec56@gmail.com>
2023-07-31 15:04:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,14 +10,17 @@ fi
 apt update
 
 # Installer les paquets Meson, Ninja et libbsd-dev
-apt install -y meson ninja-build libbsd-dev
+apt install -y meson ninja-build libbsd-dev git 
 
 # Vérifier si l'installation a réussi
 if [ $? -eq 0 ]; then
-    echo "L'installation de Meson, Ninja et libbsd-dev est terminée avec succès."
+    echo "L'installation de Meson, Ninja et libbsd-dev git  est terminée avec succès."
 else
     echo "Une erreur s'est produite lors de l'installation des paquets."
 fi
+
+echo "Repo Clone"
+git clone https://github.com/loic564492/xget/
 
 echo "Building The App ...."
 meson setup build
